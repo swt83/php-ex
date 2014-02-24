bfe77fefa7455e79fb54caa9fb81b86e6c13c682
Scott Travis <scott.w.travis@gmail.com>
2014-02-24 09:10:01 -0600

    Add function_exists() check.

--- src/ex.php
+++ src/ex.php
@@ -1,41 +1,44 @@
 <?php
 
-function ex($object, $coords, $default = null)
+if (!function_exists('ex'))
 {
-    if (!is_array($object) and !is_object($object))
+    function ex($object, $coords, $default = null)
     {
-        return $default;
-    }
-
-    $keys = explode('.', $coords);
-    foreach ($keys as $key)
-    {
-        if (is_array($object))
-        {
-            if (isset($object[$key]))
-            {
-                $object = $object[$key];
-            }
-            else
-            {
-                return $default;
-            }
-        }
-        elseif (is_object($object))
-        {
-            if (isset($object->$key))
-            {
-                $object = $object->$key;
-            }
-            else
-            {
-                return $default;
-            }
-        }
-        else
+        if (!is_array($object) and !is_object($object))
         {
             return $default;
         }
+
+        $keys = explode('.', $coords);
+        foreach ($keys as $key)
+        {
+            if (is_array($object))
+            {
+                if (isset($object[$key]))
+                {
+                    $object = $object[$key];
+                }
+                else
+                {
+                    return $default;
+                }
+            }
+            elseif (is_object($object))
+            {
+                if (isset($object->$key))
+                {
+                    $object = $object->$key;
+                }
+                else
+                {
+                    return $default;
+                }
+            }
+            else
+            {
+                return $default;
+            }
+        }
+        return $object ? $object : $default;
     }
-    return $object ? $object : $default;
 }
